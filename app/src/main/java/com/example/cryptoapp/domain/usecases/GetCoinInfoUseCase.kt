3d56a8b21042c7399ff72e2ebbccfd58e0cb3f6d
passerby29 <grtte@list.ru>
2023-06-04 17:06:05 +0300

    Lesson 1 commit

---
+++ app/src/main/java/com/example/cryptoapp/domain/usecases/GetCoinInfoUseCase.kt
@@ -0,0 +1,9 @@
+package com.example.cryptoapp.domain.usecases
+
+import com.example.cryptoapp.domain.CoinRepository
+
+class GetCoinInfoUseCase(
+    private val repository: CoinRepository
+) {
+    operator fun invoke(fromSymbol: String) = repository.getCoinInfo(fromSymbol)
+}
