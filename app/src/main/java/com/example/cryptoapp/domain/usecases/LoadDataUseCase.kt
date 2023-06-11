83932404d296019779387064d9bcb1c5875403a6
passerby29 <grtte@list.ru>
2023-06-11 17:15:34 +0300

    Lesson 8 commit

--- app/src/main/java/com/example/cryptoapp/domain/usecases/LoadDataUseCase.kt
+++ app/src/main/java/com/example/cryptoapp/domain/usecases/LoadDataUseCase.kt
@@ -6,5 +6,5 @@ class LoadDataUseCase(
     private val repository: CoinRepository
 ) {
 
-    suspend operator fun invoke() = repository.loadData()
+    operator fun invoke() = repository.loadData()
 }
--- app/src/main/java/com/example/cryptoapp/presentation/CoinViewModel.kt
+++ app/src/main/java/com/example/cryptoapp/presentation/CoinViewModel.kt
@@ -2,12 +2,10 @@ package com.example.cryptoapp.presentation
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.viewModelScope
 import com.example.cryptoapp.data.repository.CoinRepositoryImpl
 import com.example.cryptoapp.domain.usecases.GetCoinInfoListUseCase
 import com.example.cryptoapp.domain.usecases.GetCoinInfoUseCase
 import com.example.cryptoapp.domain.usecases.LoadDataUseCase
-import kotlinx.coroutines.launch
 
 class CoinViewModel(application: Application) : AndroidViewModel(application) {
 
@@ -22,8 +20,6 @@ class CoinViewModel(application: Application) : AndroidViewModel(application) {
     fun getDetailInfo(fSym: String) = getCoinInfoUseCase(fSym)
 
     init {
-        viewModelScope.launch {
-            loadDataUseCase()
-        }
+        loadDataUseCase()
     }
 }
